f8b3ba47916f1155af8c43dbb64ff44d17982e67
isacamilli <isadevprojects@gmail.com>
2025-05-26 23:45:29 -0300

    sequencia

--- src/Sequencia/Sequencia.java
+++ src/Sequencia/Sequencia.java
@@ -23,5 +23,65 @@ public class Sequencia {
         return tamanho == 0;
     }
 
-    
+    public Object elemAtRank(int p)    {
+        No n = atRank(p);
+        return n.getElemento();
+    }
+
+    public No atRank(int r){
+        No node;
+        if (r <= size()/2){
+            node = inicio.getDepois();
+            for(int i = 0;i < r;i++){
+                node = node.getDepois();
+            }
+        }
+        else{
+            node = fim.getAntes();
+            for(int i=0;i<size()-r-1;i++){
+                node = node.getAntes();
+            }
+        }
+        return node;
+    }
+
+    public int rankOf(No n){
+        No ref = inicio.getDepois();
+        int p = 0;
+
+        while (ref != n && n != fim) {
+            ref = ref.getDepois();
+            p++;
+        }
+        return p;
+    }
+
+    public Object replaceAtRank(int r, Object o){
+        No novoNo = atRank(r);
+        Object ref = novoNo.getElemento();
+        novoNo.setElemento(o);
+
+        return ref;
+    }
+
+    public void insertAtRank(int r, Object o){
+        No ref = atRank(r);
+        No novoNo = new No(o);
+
+        novoNo.setAntes(ref.getAntes());
+        novoNo.setDepois(ref);
+
+        ref.getAntes().setDepois(novoNo);
+        ref.setAntes(novoNo);
+
+        tamanho++;
+    }
+
+    public No first(){
+        return inicio.getDepois();
+    }
+
+    public No last(){
+        return fim.getAntes();
+    }
 }
